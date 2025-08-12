08743bdc0d0e13be0a2dfc12a5ba07092acc458a
wuebetmistireselassie <wuebetmistireselassie@gmail.com>
2025-08-12 22:19:25 +0300

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,15 @@
+// This file lists all the projects for your portfolio.
+// To add a new project, upload the image to your 'images' folder,
+// then add the filename to this list.
+
+const projectFiles = [
+    "A and R.png",
+    "Bean again.jpg",
+    "For comparison.jpg",
+    "Leafinity 1.png",
+    "Leafinity 2.png",
+    "Leafinity Development.png",
+    "Leafinity adv.png",
+    "May be 1.jpg",
+    "May be.jpg"
+];
