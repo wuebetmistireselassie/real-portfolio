99e33bf1d16116d1fe13697aab85eb95bad1435d
wuebetmistireselassie <wuebetmistireselassie@gmail.com>
2025-08-22 19:25:54 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-// IMPORTANT: Replace with your actual Cloudinary credentials from Step 2.
-export const CLOUDINARY_CONFIG = {
-    CLOUD_NAME: "dpnw0unbx",
-    UPLOAD_PRESET: "pss2ovaq"
-};
